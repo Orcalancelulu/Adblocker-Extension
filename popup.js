0bcd854b4c62a06cdd854ceb9bda6f169bba7232
Orcalancelulu <lanceborer1@gmail.com>
2022-07-07 10:59:15 +0200

    url richtig anzeigen

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 getLiveData();
 
-
+let buttonNetworkEnabled;
+let buttonDeleterEnabled;
 
 buttonNetwork = document.getElementById("buttonNetwork");
 buttonDeleter = document.getElementById("buttonDeleter");
 
 slider = document.getElementById("checkboxSwitch");
-slider.checked = true;
 
 chrome.declarativeNetRequest.getEnabledRulesets({rulesetIds: ["weakRules"]}).then(function (response) {
   if (response.length == 0) {
@@ -16,6 +16,7 @@ chrome.declarativeNetRequest.getEnabledRulesets({rulesetIds: ["weakRules"]}).the
     buttonNetwork.style.backgroundColor = "LightGreen";
     buttonNetworkEnabled = true;
   }
+  checkSlider();
 });
 
 chrome.runtime.sendMessage({asking: "canidelete"}, function (response) {
@@ -26,7 +27,25 @@ chrome.runtime.sendMessage({asking: "canidelete"}, function (response) {
     buttonDeleter.style.backgroundColor = "tomato";
     buttonDeleterEnabled = false;
   }
+  checkSlider(); //2 mal checkSlider, weil die Funktionen asynchron sind. Man weiss nicht, welches zuerst fertig ist, deshalb einfach 2 mal checken
 })    
+
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+  let url = tabs[0].url;
+
+  if(url.indexOf("https://") > -1) {
+    url = url.substr(8);
+  } else url = url.substr(7);
+
+  if (url.indexOf("www") > -1) {
+    url = url.substr(4);
+  }
+  urlArray = url.split("/", 1);
+  url = urlArray[0];
+  
+  urlElement = document.getElementById("siteName");
+  urlElement.innerHTML = url;  
+});
 
 
 buttonNetwork.addEventListener("click", sendNetworkMessage);
